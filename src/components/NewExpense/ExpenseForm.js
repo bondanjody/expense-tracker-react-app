09bccb05532e6df0bcdaec814d16380ed1077d51
bondanjody <bondanjodys@gmail.com>
2023-02-16 21:17:43 +0700

    Menambahkan event handler (onChange) pada textfield 'title'. Serta membuat function untuk menangkap event tersebut (titleChangeHandler) yang menangkap argumen 'event'. Lalu mengakses nilai yang diinputkan dalam textfield dengan menggunakan 'event.target.value'.

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -2,11 +2,14 @@ import React from "react";
 import './ExpenseForm.css';
 
 const ExpenseForm = () => {
+    const titleChangeHandler = (event) => {
+        console.log(event.target.value);
+    }
     return <form>
         <div className="new-expense__constrols">
             <div className="new-expense__control">
                 <label>Title</label>
-                <input type="text" />
+                <input type="text" onChange={titleChangeHandler} />
             </div>
             <div className="new-expense__control">
                 <label>Amount</label>
