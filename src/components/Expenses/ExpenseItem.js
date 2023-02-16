67c3e7f1f8fc049934bf3d24ab9cbee633b8c0ec
bondanjody <bondanjodys@gmail.com>
2023-02-16 11:26:49 +0700

    Menambahkan button Change title dan event handler nya (onClick). Penjelasan cara memanggil function / cara membuat function di dalam event handler pada JSX. Setiap function yang kita definisikan sendiri (bukan dari library lain) sebaiknya diakhiri dengan 'Handler'.

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -4,12 +4,16 @@ import Card from '../UI/Card';
 import ExpenseDate from './ExpenseDate';
 
 const ExpenseItem = (props) => {
+    const clickHandler = () => {
+        console.log("You clicked the button !");
+    }
 
     return (
         <Card className='expense-item'>
             <ExpenseDate date={props.date} />
             <h2 className="expense-item__description ">{props.title}</h2>
             <div className=" expense-item__price">${props.amount}</div>
+            <button onClick={clickHandler}>Change title</button>
         </Card>
     );
 }
