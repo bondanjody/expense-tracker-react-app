db65558a6f00f67a8960cb9805e52714cb7c8ff2
bondanjody <bondanjodys@gmail.com>
2023-02-16 08:18:59 +0700

    Menambahkan Card.js. Yang mana berfungsi untuk mengurangi duplikasi kode (CSS). Menggunakan 'prop.children'. Menggabungkan dua className.

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,14 +1,15 @@
 import './ExpenseItem.css';
+import Card from './Card';
 import ExpenseDate from './ExpenseDate';
 
 const ExpenseItem = (props) => {
 
     return (
-        <div className="expense-item">
+        <Card className='expense-item'>
             <ExpenseDate date={props.date} />
             <h2 className="expense-item__description ">{props.title}</h2>
             <div className=" expense-item__price">${props.amount}</div>
-    </div>
+        </Card>
     );
 }
 
