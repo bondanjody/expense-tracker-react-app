5b32f966880af95b91205e8620207970d6bcddc9
bondanjody <bondanjodys@gmail.com>
2023-02-16 11:55:27 +0700

    State pada React digunakan apabila kita ingin komponen yang kita buat melakukan render ulang ketika suatu objek berubah nilainya. Menggunakan useState. useState harus dipanggil di dalam function komponen kita (tidak boleh di luar function component). Melakukan destructuring dari useState berupa array. Nilai array pertama (title) digunakan untuk menyimpan nilai awal, sedangkan array yang kedua merupakan nilai perubahan.

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,17 +1,20 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './ExpenseItem.css';
 import Card from '../UI/Card';
+
 import ExpenseDate from './ExpenseDate';
 
 const ExpenseItem = (props) => {
+    const [title, setTitle] = useState(props.title);
     const clickHandler = () => {
-        console.log("You clicked the button !");
+        setTitle('Updated');
+        console.log(title);
     }
 
     return (
         <Card className='expense-item'>
             <ExpenseDate date={props.date} />
-            <h2 className="expense-item__description ">{props.title}</h2>
+            <h2 className="expense-item__description ">{title}</h2>
             <div className=" expense-item__price">${props.amount}</div>
             <button onClick={clickHandler}>Change title</button>
         </Card>
