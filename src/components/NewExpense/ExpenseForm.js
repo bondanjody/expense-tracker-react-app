d87ed197c89a755887cf637f0f5155489965f32b
bondanjody <bondanjodys@gmail.com>
2023-02-17 12:40:26 +0700

    Memungkinkan form yang sudah di submit akan kosong kembali (me reset) dengan prinsip Two-way data binding. Mengset value tiap elemen (texfield) dengan (titleChange, amountChange, dateChange).

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -61,21 +61,26 @@ const ExpenseForm = () => {
             date: new Date(dateChange)
         };
         console.log(expense);
+
+        // me reset (mengosongkan isi form) ketika form di submit
+        setTitleChange('');
+        setAmountChange('');
+        setDateChange('');
     }
 
     return <form onSubmit={submitHandler}>
         <div className="new-expense__constrols">
             <div className="new-expense__control">
                 <label>Title</label>
-                <input type="text" onChange={titleChangeHandler} />
+                <input type="text" value={titleChange} onChange={titleChangeHandler} />
             </div>
             <div className="new-expense__control">
                 <label>Amount</label>
-                <input type="number" min="0.01" step="0.01" onChange={amountChangeHandler} />
+                <input type="number" min="0.01" step="0.01" value={amountChange} onChange={amountChangeHandler} />
             </div>
             <div className="new-expense__control">
                 <label>Date</label>
-                <input type="date" min="2023-01-01" max="2024-01-01" onChange={dateChangeHandler} />
+                <input type="date" min="2023-01-01" max="2024-01-01" value={dateChange} onChange={dateChangeHandler} />
             </div>
             <div className="new-expense__actions">
                 <button type="submit">Add Expense</button>
