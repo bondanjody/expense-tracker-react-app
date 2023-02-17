a68dcfc5f26e5835b278b06f15a48eb03cea0860
bondanjody <bondanjodys@gmail.com>
2023-02-17 16:55:08 +0700

    Menambahkan komponen baru : ExpensesFilter.js yang berfungsi untuk melakukan filter tahun. Tetapi komponen belum berfungsi secara sempurna. Menerapkan Lifting-up mengirim data dari child komponen ke parent komponen. Menerapkan two-way binding.

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,11 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import Card from "../UI/Card";
 import ExpenseItem from "./ExpenseItem";
 import './Expenses.css';
+import ExpensesFilter from "./ExpensesFilter";
 
 const Expenses = props => {
+    const [filteredYear, setFilteredYear] = useState('2020');
+    const filterChangeHandler = selectedYear => {
+        setFilteredYear(selectedYear);
+    }
     return(
         <Card className="expenses">
+            <ExpensesFilter selected={filteredYear} onFilterChange={filterChangeHandler} />
             <ExpenseItem title={props.items[0].title} amount={props.items[0].amount} date={props.items[0].date} />
             <ExpenseItem title={props.items[1].title} amount={props.items[1].amount} date={props.items[1].date} />
             <ExpenseItem title={props.items[2].title} amount={props.items[2].amount} date={props.items[2].date} />
