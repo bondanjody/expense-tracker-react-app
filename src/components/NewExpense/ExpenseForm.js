7f7487f4f5c927582ebc57cd73d7699fc3877b4a
bondanjody <bondanjodys@gmail.com>
2023-02-17 07:21:12 +0700

    Menambahkan function handler untuk date dan amount. Menambahkan useState untuk title, amount, dan date.

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -1,10 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import './ExpenseForm.css';
 
 const ExpenseForm = () => {
+    const [titleChange, setTitleChange] = useState('');
+    const [amountChange, setAmountChange] = useState('');
+    const [dateChange, setDateChange] = useState('');
+
     const titleChangeHandler = (event) => {
-        console.log(event.target.value);
+        setTitleChange(event.target.value);
     }
+    const amountChangeHandler = (event) => {
+        setAmountChange(event.target.value);
+    }
+    const dateChangeHandler = (event) => {
+        setDateChange(event.target.value);
+    }
+
     return <form>
         <div className="new-expense__constrols">
             <div className="new-expense__control">
@@ -13,11 +24,11 @@ const ExpenseForm = () => {
             </div>
             <div className="new-expense__control">
                 <label>Amount</label>
-                <input type="number" min="0.01" step="0.01" />
+                <input type="number" min="0.01" step="0.01" onChange={amountChangeHandler} />
             </div>
             <div className="new-expense__control">
                 <label>Date</label>
-                <input type="date" min="2023-01-01" max="2024-01-01" />
+                <input type="date" min="2023-01-01" max="2024-01-01" onChange={dateChangeHandler} />
             </div>
             <div className="new-expense__actions">
                 <button type="submit">Add Expense</button>
