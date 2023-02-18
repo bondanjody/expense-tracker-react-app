c9225369d05896ed0d4bcf47dbe56050cb9f1b16
bondanjody <bondanjodys@gmail.com>
2023-02-18 11:33:42 +0700

    Stateful list. Mengubah nama expenses pada file App.js menjadi DUMMY_EXPENSES. Menggunakan useState untuk menangani inputan list (DUMMY_EXPENSES) supaya dapat ditambahkan dari inputan kita

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import React from "react";
+import React, {useState} from "react";
 import NewExpense from "./components/NewExpense/NewExpense";
 import Expenses from "./components/Expenses/Expenses";
 
 function App() {
-  const expenses = [
+  const DUMMY_EXPENSES = [
     {
         id: 'e1', title: 'New Phone', amount: 34, date: new Date(2023, 2, 1)
     },
@@ -18,9 +18,12 @@ function App() {
     }
     ];
 
+    const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
+
   const addExpenseHandler = expense => {
-    console.log('In App.js');
-    console.log(expense);
+    setExpenses(prevExpenses => {
+      return [expense, ...prevExpenses];
+    })
   }
 
   return (
