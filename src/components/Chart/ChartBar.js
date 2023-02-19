270bd3e3fc8528897d14d3ac50932cbc6ee04116
bondanjody <bondanjodys@gmail.com>
2023-02-19 08:58:28 +0700

    Memodifikasi file ChartBar.js yang meliputi : menentukan barFilledHeight dan mensetting height suatu elemen sesuai dengan barFilledHeight yaitu dengan menggunakan 'dynamic styling' yang menyimpan objek CSS di dalam komponen JSX.

--- src/components/Chart/ChartBar.js
+++ src/components/Chart/ChartBar.js
@@ -1,14 +1,20 @@
 import React from "react";
 import './ChartBar.css';
 
-const ChartBar = () => {
+const ChartBar = props => {
+    const barFilledHeight = '0%';
+
+    if(props.max > 0){
+        barFilledHeight = Math.round((props.value / props.maxValue)*100);
+    }
+
     return (
     <div className="chart-bar">
         <div className="chart-bar__inner">
-            <div className="chart-bar__fill"></div>
+            <div className="chart-bar__fill" style={{ height : barFilledHeight }}></div>
         </div>
         <div className="chart-bar__label">
-
+            {props.label}
         </div>
     </div>);
 }
